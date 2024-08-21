243a207db0e2925377cb0a522856d2a903dba476
SyntaxSavvy504 <utsavk528@gmail.com>
2024-08-21 23:42:40 +0530

    Update manager.py
    fix it

--- manager/manager.py
+++ manager/manager.py
@@ -87,7 +87,7 @@ class Manager(commands.Cog):
             embed.add_field(name="Purchase Date", value=f"> {purchase_date}", inline=False)
             embed.add_field(name="\u200b", value="**- follow our [TOS](https://discord.com/channels/911622571856891934/911629489325355049) & be a smart buyer!\n- [CLICK HERE](https://discord.com/channels/911622571856891934/1134197532868739195) to leave your __feedback__**", inline=False)
             embed.add_field(name="Product info and credentials", value=f"||```{custom_text}```||", inline=False)
-            embed.add_field(name="__Vouch Format__", value=f"||```{vouch_text}```||", inline=False)
+            embed.add_field(name="__Vouch Format__", value=f"```{vouch_text}```", inline=False)
             embed.set_footer(text=f"__Thanks for order. No vouch, no warranty__")
             embed.set_image(url="https://media.discordapp.net/attachments/1271370383735394357/1271370426655703142/931f5b68a813ce9d437ec11b04eec649.jpg")
 
